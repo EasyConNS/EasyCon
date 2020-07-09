a9894350d284496c479eef9951aabfdb48395137
elmagnificoco <elmagnificogg@gmail.com>
2020-07-10 01:59:27 +0800

    Add capture video form

---
+++ EasyCon/CaptureVideo.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace EasyCon
+{
+    partial class CaptureVideo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VideoSourcePlayerMonitor = new AForge.Controls.VideoSourcePlayer();
+            this.SuspendLayout();
+            // 
+            // VideoSourcePlayerMonitor
+            // 
+            this.VideoSourcePlayerMonitor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.VideoSourcePlayerMonitor.Enabled = false;
+            this.VideoSourcePlayerMonitor.Location = new System.Drawing.Point(0, 0);
+            this.VideoSourcePlayerMonitor.Margin = new System.Windows.Forms.Padding(13);
+            this.VideoSourcePlayerMonitor.Name = "VideoSourcePlayerMonitor";
+            this.VideoSourcePlayerMonitor.Size = new System.Drawing.Size(800, 450);
+            this.VideoSourcePlayerMonitor.TabIndex = 0;
+            this.VideoSourcePlayerMonitor.Text = "videoSourcePlayer1";
+            this.VideoSourcePlayerMonitor.VideoSource = null;
+            // 
+            // CaptureVideo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VideoSourcePlayerMonitor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CaptureVideo";
+            this.Text = "CaptureVideo";
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.CaptureVideo_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CaptureVideo_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CaptureVideo_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.CaptureVideo_MouseUp);
+            this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.CaptureVideo_MouseWheel);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer VideoSourcePlayerMonitor;
+    }
+}
---
+++ EasyCon/CaptureVideo.cs
@@ -0,0 +1,87 @@
+﻿using AForge.Video;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace EasyCon
+{
+    public partial class CaptureVideo : Form
+    {
+        bool isMouseDown = false;
+        Point mouseOffset;
+        public CaptureVideo()
+        {
+            InitializeComponent();
+        }
+
+        public CaptureVideo(IVideoSource source)
+        {
+            InitializeComponent();
+            VideoSourcePlayerMonitor.VideoSource = source;
+        }
+
+        public void setVideoSource(IVideoSource source)
+        {
+            VideoSourcePlayerMonitor.VideoSource = source;
+        }
+
+        private void CaptureVideo_MouseDown(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("mouse down12");
+            if (e.Button == MouseButtons.Left)
+            {
+                Control control = sender as Control;
+                int offsetX = -e.X;
+                int offsetY = -e.Y;
+                //判断是窗体还是控件，从而改进鼠标相对于窗体的位置
+                if (!(control is System.Windows.Forms.Form))
+                {
+                    offsetX = offsetX - control.Left;
+                    offsetY = offsetY - control.Top;
+                }
+                mouseOffset = new Point(offsetX, offsetY);
+                isMouseDown = true;
+                Debug.WriteLine("mouse down" + offsetX + " " + offsetY);
+            }
+        }
+
+        private void CaptureVideo_MouseUp(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("123");
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+                Debug.WriteLine("mouse up");
+            }
+        }
+
+        private void CaptureVideo_MouseMove(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("44");
+            if (isMouseDown)
+            {
+                Point mouse = Control.MousePosition;
+                mouse.Offset(mouseOffset.X, mouseOffset.Y);
+                Debug.WriteLine("mouse move" + mouseOffset.X + " " + mouseOffset.Y);
+                this.Location = mouse;
+            }
+        }
+
+        private void CaptureVideo_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CaptureVideo_MouseWheel(object sender, MouseEventArgs e)
+        {
+            //this.Close();
+        }
+    }
+}
