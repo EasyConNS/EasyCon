99c15e5a81254771a1f0e31e9f81236e16d2fef5
ca1e <i@scp500.com>
2022-06-19 19:24:02 +0800

    调整默认固件显示顺序

--- EasyCon2/Helper/Board.cs
+++ EasyCon2/Helper/Board.cs
@@ -8,11 +8,11 @@
         public readonly int DataSize;
 
         public static Board[] SupportedBoards = new Board[] {
-            new ("Arduino UNO R3", "UNO", 412),
-            new ("Beetle", "Beetle", 924),
             new ("Leonardo", "Leonardo", 924),
             new("Teensy 2.0", "Teensy2", 924),
             new("Teensy 2.0++", "Teensy2pp", 3996),
+            new ("Beetle", "Beetle", 924),
+            new ("Arduino UNO R3", "UNO", 412),
             };
 
         public Board(string displayname, string corename, int datasize)
