63f40d63860bbcc19e544158875ad64a9738bb1f
elmagnifico <elmagnificogg@gmail.com>
2022-08-05 00:17:20 +0800

    add UNO and STM32 support

--- EasyCon2/Helper/Board.cs
+++ EasyCon2/Helper/Board.cs
@@ -12,7 +12,8 @@
             new("Teensy 2.0", "Teensy2", 924),
             new("Teensy 2.0++", "Teensy2pp", 3996),
             new ("Beetle", "Beetle", 924),
-           // new ("Arduino UNO R3", "UNO", 412),
+            new ("Arduino UNO R3", "UNO", 412),
+            new ("STM32F103C6T6", "STM32F103C6T6", 924),
             };
 
         public Board(string displayname, string corename, int datasize)
