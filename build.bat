c981d95e36caf939d44cf44eac5c5848df0f4bce
elmagnifico <elmagnificogg@gmail.com>
2022-09-20 16:27:29 +0800

    change amiibo images location

--- build.bat
+++ build.bat
@@ -15,7 +15,6 @@ for /F %%i in ('git rev-parse --short HEAD') do ( set commitid=%%i)
 ren EasyCon2.exe EasyCon.net6.0.%commitid%.exe
 cd ../
 xcopy .\Firmware .\publish\Firmware\
-xcopy .\Amiibo\AmiiboImages .\publish\Amiibo\AmiiboImages\
 xcopy .\Script .\publish\Script\
 
 pause
