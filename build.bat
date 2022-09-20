baf1394cc8b3cf88f845d6fda20c75338a0c8861
elmagnifico <elmagnificogg@gmail.com>
2022-09-21 00:44:41 +0800

    fix release no amiibo images

--- build.bat
+++ build.bat
@@ -16,5 +16,5 @@ ren EasyCon2.exe EasyCon.net6.0.%commitid%.exe
 cd ../
 xcopy .\Firmware .\publish\Firmware\
 xcopy .\Script .\publish\Script\
-
+xcopy .\EasyCon2\Amiibo\AmiiboImages .\publish\Amiibo\AmiiboImages\
 pause
