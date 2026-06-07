2265a44f77fe7b359170194e4e067cffb88d549f
Salt-Pepper-Seal <salt-pepper-seal@outlook.com>
2026-06-07 13:24:40 +0800

    标签编辑器界面优化

--- src/EasyCon2.Avalonia.Core/TagEditor/TagEditorViewModel.cs
+++ src/EasyCon2.Avalonia.Core/TagEditor/TagEditorViewModel.cs
@@ -24,6 +24,53 @@ public partial class TagEditorViewModel : ObservableObject
 
     public static readonly IReadOnlyList<SearchMethod> SearchMethods = ECCore.GetSearchMethods().ToList();
 
+    public static readonly IReadOnlyList<string> ParameterOptions =
+    [
+        "默认",
+        "灰度",
+        "二值",
+        "高斯模糊",
+        "其他"
+    ];
+
+    public string SelectedParameterDisplay
+    {
+        get
+        {
+            var selected = new List<string>();
+            if (UseGrayscale)
+                selected.Add("灰度");
+            if (UseBinary)
+                selected.Add("二值");
+            if (UseGaussianBlur)
+                selected.Add("高斯模糊");
+            if (UseOther)
+                selected.Add("其他");
+
+            return string.Join("+", selected);
+        }
+    }
+
+    public double SelectedParameterFontSize
+    {
+        get
+        {
+            var length = SelectedParameterDisplay.Length;
+            if (length <= 4)
+                return 15;
+            if (length <= 7)
+                return 13;
+
+            return 11;
+        }
+    }
+
+    public int ParameterComboSelectedIndex
+    {
+        get => -1;
+        set => OnPropertyChanged();
+    }
+
     #region Proxy properties from ImgLabel
 
     [ObservableProperty]
@@ -79,22 +126,55 @@ public partial class TagEditorViewModel : ObservableObject
     [ObservableProperty]
     private bool _useGrayscale;
 
-    partial void OnUseGrayscaleChanged(bool value) => Label.UseGrayscale = value;
+    partial void OnUseGrayscaleChanged(bool value)
+    {
+        Label.UseGrayscale = value;
+        OnSelectedParametersChanged();
+    }
 
     [ObservableProperty]
     private bool _useBinary;
 
-    partial void OnUseBinaryChanged(bool value) => Label.UseBinary = value;
+    partial void OnUseBinaryChanged(bool value)
+    {
+        Label.UseBinary = value;
+        OnSelectedParametersChanged();
+    }
 
     [ObservableProperty]
     private bool _useGaussianBlur;
 
-    partial void OnUseGaussianBlurChanged(bool value) => Label.UseGaussianBlur = value;
+    partial void OnUseGaussianBlurChanged(bool value)
+    {
+        Label.UseGaussianBlur = value;
+        OnSelectedParametersChanged();
+    }
 
     [ObservableProperty]
     private bool _useOther;
 
-    partial void OnUseOtherChanged(bool value) => Label.UseOther = value;
+    partial void OnUseOtherChanged(bool value)
+    {
+        Label.UseOther = value;
+        OnSelectedParametersChanged();
+    }
+
+    private void OnSelectedParametersChanged()
+    {
+        OnPropertyChanged(nameof(SelectedParameterDisplay));
+        OnPropertyChanged(nameof(SelectedParameterFontSize));
+    }
+
+    [ObservableProperty]
+    private string _selectedParameterOption = "默认";
+
+    partial void OnSelectedParameterOptionChanged(string value)
+    {
+        UseGrayscale = value == "灰度";
+        UseBinary = value == "二值";
+        UseGaussianBlur = value == "高斯模糊";
+        UseOther = value == "其他";
+    }
 
     #endregion
 
@@ -121,6 +201,7 @@ public partial class TagEditorViewModel : ObservableObject
         UseBinary = label.UseBinary;
         UseGaussianBlur = label.UseGaussianBlur;
         UseOther = label.UseOther;
+        SelectedParameterOption = GetParameterOption();
 
         TargetImage = CreateTargetImage(label);
     }
@@ -135,4 +216,63 @@ public partial class TagEditorViewModel : ObservableObject
     }
 
     public ImgLabel ToImgLabel() => Label with { };
+
+    [RelayCommand]
+    private void IncrementCoordinate(string name)
+    {
+        AdjustCoordinate(name, 1);
+    }
+
+    [RelayCommand]
+    private void DecrementCoordinate(string name)
+    {
+        AdjustCoordinate(name, -1);
+    }
+
+    private void AdjustCoordinate(string name, int delta)
+    {
+        static int Clamp(int value) => Math.Clamp(value, 0, 9999);
+
+        switch (name)
+        {
+            case nameof(TargetX):
+                TargetX = Clamp(TargetX + delta);
+                break;
+            case nameof(TargetY):
+                TargetY = Clamp(TargetY + delta);
+                break;
+            case nameof(TargetWidth):
+                TargetWidth = Clamp(TargetWidth + delta);
+                break;
+            case nameof(TargetHeight):
+                TargetHeight = Clamp(TargetHeight + delta);
+                break;
+            case nameof(RangeX):
+                RangeX = Clamp(RangeX + delta);
+                break;
+            case nameof(RangeY):
+                RangeY = Clamp(RangeY + delta);
+                break;
+            case nameof(RangeWidth):
+                RangeWidth = Clamp(RangeWidth + delta);
+                break;
+            case nameof(RangeHeight):
+                RangeHeight = Clamp(RangeHeight + delta);
+                break;
+        }
+    }
+
+    private string GetParameterOption()
+    {
+        if (UseGrayscale)
+            return "灰度";
+        if (UseBinary)
+            return "二值";
+        if (UseGaussianBlur)
+            return "高斯模糊";
+        if (UseOther)
+            return "其他";
+
+        return "默认";
+    }
 }
